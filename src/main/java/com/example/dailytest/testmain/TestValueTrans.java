675f8958c6e23dea1d11cfcf55df00f3671d982a
Sure <wangfengshuo2012@163.com>
2020-05-17 19:01:47 +0800

    【完善】Java 值传递小试

--- src/main/java/com/example/dailytest/testmain/TestValueTrans.java
+++ src/main/java/com/example/dailytest/testmain/TestValueTrans.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
  * @author: w15021
  * @create: 2020-04-01
  **/
+/*
 public class TestValueTrans {
     public static JSONObject changeObj (JSONObject oldObj){
         Integer code = oldObj.getIntValue("code");
@@ -36,16 +37,12 @@ public class TestValueTrans {
         System.out.println(oldObj.remove("no"));
         //oldObj.remove("no");
 
-
         JSONObject trueResult = oldObj;
 
         JSONObject changedResultObj = new JSONObject();
         changedResultObj.put("code",code);
         changedResultObj.put("data",trueResult);
         return changedResultObj;
-
-
-
     }
 
     public static void main(String[] args) {
@@ -55,9 +52,54 @@ public class TestValueTrans {
         System.out.println(oldObj.toJSONString());
         oldObj = changeObj(oldObj);
         System.out.println(oldObj.toString());
+    }
+}*/
 
+public class TestValueTrans {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("小张");
+        Student s2 = new Student("小李");
+        TestValueTrans.swap(s1, s2);
+        System.out.println("s1:" + s1.getName());
+        System.out.println("s2:" + s2.getName());
+
+        //一个方法不能让对象参数引用一个新的对象。
+        changeName(s1);
+        System.out.println(s1.getName());
     }
 
+    public static void swap(Student x, Student y) {
+        Student temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x:" + x.getName());
+        System.out.println("y:" + y.getName());
+    }
 
+    public static void changeName (Student student) {
+        student.setName("小王");
+    }
 
+    public static class Student {
+        private String name;
+
+        Student (String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+
+/*
+一个方法不能修改一个基本数据类型的参数（即数值型或布尔型）。
+一个方法可以改变一个对象参数的状态。
+一个方法不能让对象参数引用一个新的对象。
+ */
