ceec806fa443b9178a6a0ac4a1bd96ed1b6aa4c7
Sure <wangfengshuo2012@163.com>
2020-05-17 18:12:53 +0800

    【新增】equals方法使用

---
+++ src/main/java/com/example/dailytest/testmain/TestEquel.java
@@ -0,0 +1,19 @@
+package com.example.dailytest.testmain;
+
+public class TestEquel {
+    public static void main(String[] args) {
+        String a = new String("ab"); // a 为⼀个引⽤
+        String b = new String("ab"); // b为另⼀个引⽤,对象的内容⼀样
+        String aa = "ab"; // 放在常量池中
+        String bb = "ab"; // 从常量池中查找
+        if (aa == bb) // true
+            System.out.println("aa==bb");
+        if (a == b) // false，⾮同⼀对象
+            System.out.println("a==b");
+        if (a.equals(b)) // true  (String 的euqel是被重写过的，是比较对象的内容)
+            System.out.println("aEQb");
+        if (42 == 42.0) { // true
+            System.out.println("true");
+        }
+    }
+}
