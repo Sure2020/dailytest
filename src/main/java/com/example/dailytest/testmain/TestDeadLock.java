893dce21aa3eb0d9e354d052e7958b58ff849279
yys1857 <ys.yunanxing@h3c.com>
2020-05-21 20:07:47 +0800

    死锁现象之破坏循环等待条件以避免死锁

--- src/main/java/com/example/dailytest/testmain/TestDeadLock.java
+++ src/main/java/com/example/dailytest/testmain/TestDeadLock.java
@@ -45,7 +45,8 @@ public class TestDeadLock {
             }
         }, "线程 1").start();
 
-        new Thread(() -> {
+        //死锁
+        /*new Thread(() -> {
             synchronized (resource2) {
                 System.out.println(Thread.currentThread() + "get resource2");
                 try {
@@ -56,6 +57,22 @@ public class TestDeadLock {
                 System.out.println(Thread.currentThread() + "waiting get resource1");
                 synchronized (resource1) {
                     System.out.println(Thread.currentThread() + "get resource1");
+                }
+            }
+        }, "线程 2").start();*/
+
+        //不死锁
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource2");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
                 }
             }
         }, "线程 2").start();
