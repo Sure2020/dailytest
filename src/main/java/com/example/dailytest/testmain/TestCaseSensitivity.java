284067d735112fd04f317868f529b2c0ae8be627
Sure2020 <wangfengshuo2012@163.com>
2020-05-21 17:16:49 +0800

    【新增】测试变量大小写敏感，结论为敏感

---
+++ src/main/java/com/example/dailytest/testmain/TestCaseSensitivity.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : TestCaseSensitivity
+ * Date Created : 2020-05-21
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2020-05-21       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+package com.example.dailytest.testmain;
+
+/**
+ * @program: com.example.dailytest.testmain
+ * @description: xxx
+ * @author: w15021
+ * @create: 2020-05-21
+ **/
+public class TestCaseSensitivity {
+    public static void main(String[] args) {
+        String testStrA = "A";
+        String testStra = "a";
+        System.out.println(testStrA);
+        System.out.println(testStra);
+    }
+}
