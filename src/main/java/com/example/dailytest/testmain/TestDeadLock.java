39063dbb34231468c1b2e297aae52d9a9eb51a61
yys1857 <ys.yunanxing@h3c.com>
2020-05-21 19:58:44 +0800

    【新增】死锁现象演示

---
+++ src/main/java/com/example/dailytest/testmain/TestDeadLock.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : TestDeadLock
+ * Date Created : 2020-05-21
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2020-05-21       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+package com.example.dailytest.testmain;
+
+/**
+ * @program: com.example.dailytest.testmain
+ * @description: xxx
+ * @author: w15021
+ * @create: 2020-05-21
+ **/
+public class TestDeadLock {
+    //资源 1
+    private static Object resource1 = new Object();
+    //资源 2
+    private static Object resource2 = new Object();
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println(Thread.currentThread() + "get resource1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource2");
+                synchronized (resource2) {
+                    System.out.println(Thread.currentThread() + "get resource2");
+                }
+            }
+        }, "线程 1").start();
+
+        new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println(Thread.currentThread() + "get resource2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread() + "waiting get resource1");
+                synchronized (resource1) {
+                    System.out.println(Thread.currentThread() + "get resource1");
+                }
+            }
+        }, "线程 2").start();
+    }
+}
