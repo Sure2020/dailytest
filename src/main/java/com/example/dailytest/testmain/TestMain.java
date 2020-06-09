c74817682d4fd66977475dc110a48dee895a262a
Sure2020 <wangfengshuo2012@163.com>
2020-06-09 20:35:12 +0800

    【新增】在Main函数之前执行语句

---
+++ src/main/java/com/example/dailytest/testmain/TestMain.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : TestMain
+ * Date Created : 2020-06-09
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2020-06-09       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+package com.example.dailytest.testmain;
+
+/**
+ * @program: com.example.dailytest.testmain
+ * @description: xxx
+ * @author: w15021
+ * @create: 2020-06-09
+ **/
+public class TestMain {
+    /*static {
+        System.out.println("Before Main!");
+    }*/
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+    }
+    static {
+        System.out.println("Before Main!");
+    }
+}
