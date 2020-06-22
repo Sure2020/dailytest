16e0ea8c02afba4a68bd31b3128b9d3d126e0010
Sure2020 <wangfengshuo2012@163.com>
2020-06-22 14:02:46 +0800

    【新增】Scheduled 使用

--- src/main/java/com/example/dailytest/DailyTestApplication.java
+++ src/main/java/com/example/dailytest/DailyTestApplication.java
@@ -2,8 +2,10 @@ package com.example.dailytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DailyTestApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/dailytest/test/TestScheduled.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : TestScheduled
+ * Date Created : 2020-06-19
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2020-06-19       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+package com.example.dailytest.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: com.example.dailytest.test
+ * @description: xxx
+ * @author: w15021
+ * @create: 2020-06-19
+ **/
+@Slf4j
+@Component
+public class TestScheduled {
+    @Scheduled(cron = "*/1 * * * * ?")
+    public void testSchedule () {
+        log.error("testing schedule");
+    }
+}
