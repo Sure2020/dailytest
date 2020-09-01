909c54ae322d5194f7c2c67840f0cc04b9c06b84
Sure2020 <wangfengshuo2012@163.com>
2020-09-01 17:21:23 +0800

    【新增】String.splic方法使用

---
+++ src/main/java/com/example/dailytest/testmain/TestStringSplit.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : TestStringSplit
+ * Date Created : 2020-09-01
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2020-09-01       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+package com.example.dailytest.testmain;
+
+/**
+ * @program: com.example.dailytest.testmain
+ * @description: xxx
+ * @author: w15021
+ * @create: 2020-09-01
+ **/
+public class TestStringSplit {
+    public static void main(String[] args) {
+        String sourceStr = "mysql://localhost:1521/orcl/view/yjs/user/password";
+        String[] stringArray = sourceStr.split("\\/");
+        for (String str : stringArray) {
+            System.out.print(str + " ");
+        }
+        System.out.println("###");
+        StringBuilder sb = new StringBuilder();
+        sb.append(stringArray[2]);
+        sb.append("/");
+        sb.append(stringArray[3]);
+        String dbUrl = sb.toString();
+        System.out.println(dbUrl);
+    }
+}
